ff74b45d463ba3843e0256810a27833c72cfdee6
Amrut Patil <apatil@emmes.com>
2019-07-05 14:38:59 -0400

    Restricting Access with Interfaces

--- maps/src/Company.ts
+++ maps/src/Company.ts
@@ -7,6 +7,7 @@ export class Company {
     lat: number;
     lng: number;
   };
+  color: string = 'red';
 
   constructor() {
     this.companyName = faker.company.companyName();
@@ -15,5 +16,14 @@ export class Company {
       lat: parseFloat(faker.address.latitude()),
       lng: parseFloat(faker.address.longitude())
     };
+  }
+
+  markerContent(): string {
+    return `
+      <div>
+        <h1>Company Name: ${this.companyName}</h1>
+        <h3>Catch Phrase: ${this.catchPhrase}</h3>
+      </div>
+    `;
   }
 }
