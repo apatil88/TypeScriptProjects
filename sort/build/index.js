b45e52ed3fa5dd3142d5f8813b66489c03cce898
apatil88 <amrut.patil88@hotmail.com>
2019-07-07 18:12:26 -0400

    Adding Interface definition

--- sort/build/index.js
+++ sort/build/index.js
@@ -1,7 +1,7 @@
 "use strict";
 var Sorter_1 = require("./Sorter");
 var NumbersCollections_1 = require("./NumbersCollections");
-var numbersCollection = new NumbersCollections_1.NumbersCollection([10000, 3, -5, 0]);
+var numbersCollection = new NumbersCollections_1.NumbersCollection([50, 3, -5, 0]);
 var sorter = new Sorter_1.Sorter(numbersCollection);
 sorter.sort();
 console.log(numbersCollection.data);
