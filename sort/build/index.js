85a8749d6f32d9c8b5ffd6ee37f1d234af9911a3
apatil88 <amrut.patil88@hotmail.com>
2019-07-06 20:41:56 -0400

    Initial commit: Bad Approach

---
+++ sort/build/index.js
@@ -0,0 +1,19 @@
+var Sorter = (function () {
+    function Sorter(collection) {
+        this.collection = collection;
+    }
+    Sorter.prototype.sort = function () {
+        var length = this.collection.length;
+        for (var i = 0; i < length; i++) {
+            for (var j = 0; j < length - i - 1; j++) {
+                var temp = this.collection[j];
+                this.collection[j] = this.collection[j + 1];
+                this.collection[j + 1] = temp;
+            }
+        }
+    };
+    return Sorter;
+}());
+var sorter = new Sorter([10, 3, -5, 0]);
+sorter.sort();
+console.log(sorter.collection);
