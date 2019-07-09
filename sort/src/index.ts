9cc65b8e815447dfabc4dea2b0d5fdf256511394
apatil88 <amrut.patil88@hotmail.com>
2019-07-08 20:26:43 -0400

    LinkedList implementation

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,6 +1,7 @@
 import { Sorter } from "./Sorter";
 import { NumbersCollection } from "./NumbersCollections";
 import { CharactersCollection } from "./CharactersCollection";
+import { LinkedList } from "./LinkedList";
 
 const numbersCollection = new NumbersCollection([50, 3, -5, 0]);
 const numbersSorter = new Sorter(numbersCollection);
@@ -11,3 +12,13 @@ const charactersCollection = new CharactersCollection("Xaayb");
 const characterSorter = new Sorter(charactersCollection);
 characterSorter.sort();
 console.log(charactersCollection.data);
+
+const linkedList = new LinkedList();
+linkedList.add(-100);
+linkedList.add(5);
+linkedList.add(0);
+linkedList.add(200);
+
+const linkedListSorter = new Sorter(linkedList);
+linkedListSorter.sort();
+linkedList.print();
