ab6349033ba118a1ddacd1f992554a8174098d07
Amrut Patil <apatil@emmes.com>
2019-07-09 14:18:35 -0400

    Loading, parsing, analyzing reporting CSV files

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,1 +1,27 @@
-console.log('Hi there!');
+import * as fs from 'fs';
+
+const matches = fs
+  .readFileSync('football.csv', {
+    encoding: 'utf-8'
+  })
+  .split('\n')
+  .map(
+    (row: string): string[] => {
+      return row.split(',');
+    }
+  );
+
+const homeWin = 'H';
+const awayWin = 'A';
+const draw = 'D';
+let manUnitedWins = 0;
+
+for (let match of matches) {
+  if (match[1] === 'Man United' && match[5] === homeWin) {
+    manUnitedWins++;
+  } else if (match[2] === 'Man United' && match[5] === awayWin) {
+    manUnitedWins++;
+  }
+}
+
+console.log(`Man United won ${manUnitedWins} games`);
