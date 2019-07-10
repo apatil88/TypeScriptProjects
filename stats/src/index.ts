769ba6307c1e95e1c6c950601cce57fa5a65d313
Amrut Patil <apatil@emmes.com>
2019-07-10 11:54:49 -0400

    Using file reader to read contents of csv file

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,26 +1,13 @@
 import * as fs from 'fs';
+import { MatchResult } from './MatchResult';
+import { CsvFileReader } from './CsvFileReader';
 
-const matches = fs
-  .readFileSync('football.csv', {
-    encoding: 'utf-8'
-  })
-  .split('\n')
-  .map(
-    (row: string): string[] => {
-      return row.split(',');
-    }
-  );
-
-//enum - enumeration
-enum MatchResult {
-  HomeWin = 'H',
-  AwayWin = 'A',
-  Draw = 'D'
-}
+const reader = new CsvFileReader('football.csv');
+reader.read();
 
 let manUnitedWins = 0;
 
-for (let match of matches) {
+for (let match of reader.data) {
   if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
     manUnitedWins++;
   } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
