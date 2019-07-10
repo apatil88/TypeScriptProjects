a74b46acb65dc446263e9ed376472f0b191073a0
Amrut Patil <apatil@emmes.com>
2019-07-10 13:54:47 -0400

    Using abstract classes and generics

--- stats/src/CsvFileReader.ts
+++ stats/src/CsvFileReader.ts
@@ -1,14 +1,12 @@
 import * as fs from 'fs';
-import { dateStringToDate } from './utils';
-import { MatchResult } from './MatchResult';
 
-//Tuple
-type MatchData = [Date, string, string, number, number, MatchResult, string];
-
-export class CsvFileReader {
-  data: MatchData[] = []; //Array of Tuples
+//Generics
+export abstract class CsvFileReader<T> {
+  data: T[] = []; //Array of Tuples
 
   constructor(public filename: string) {}
+
+  abstract mapRow(row: string[]): T;
 
   read(): void {
     this.data = fs
@@ -21,18 +19,6 @@ export class CsvFileReader {
           return row.split(',');
         }
       )
-      .map(
-        (row: string[]): MatchData => {
-          return [
-            dateStringToDate(row[0]),
-            row[1],
-            row[2],
-            parseInt(row[3]),
-            parseInt(row[4]),
-            row[5] as MatchResult, //Type Assertion
-            row[6]
-          ];
-        }
-      );
+      .map(this.mapRow);
   }
 }
--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,8 +1,7 @@
-import * as fs from 'fs';
+import { MatchReader } from './MatchReader';
 import { MatchResult } from './MatchResult';
-import { CsvFileReader } from './CsvFileReader';
 
-const reader = new CsvFileReader('football.csv');
+const reader = new MatchReader('football.csv');
 reader.read();
 
 let manUnitedWins = 0;
