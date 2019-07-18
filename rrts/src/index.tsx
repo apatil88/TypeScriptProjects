761ba3ad8f4c07513a496b3b51c665a89fe81a48
Amrut Patil <apatil@emmes.com>
2019-07-18 13:42:42 -0400

    Adding component state

--- rrts/src/index.tsx
+++ rrts/src/index.tsx
@@ -1,16 +1,39 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 interface AppProps {
-  color: string;
+  color?: string;
 }
 
-class App extends React.Component<AppProps>{
-  render(){
-    return <div>
-      {this.props.color}
-    </div>
+interface AppState {
+  counter: number;
+}
+
+class App extends React.Component<AppProps, AppState> {
+  //state = { counter: 0 }; //Overrides state property defined in Component
+
+  constructor(props: AppProps) {
+    super(props);
+    this.state = { counter: 0 };
+  }
+
+  onIncrement = (): void => {
+    this.setState({ counter: this.state.counter + 1 });
+  };
+
+  onDecrement = (): void => {
+    this.setState({ counter: this.state.counter - 1 });
+  };
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.onIncrement}>Increment</button>
+        <button onClick={this.onDecrement}>Decrement</button>
+        {this.state.counter}
+      </div>
+    );
   }
 }
 
-ReactDOM.render(<App color="red"/>, document.querySelector('#root'));
+ReactDOM.render(<App color="red" />, document.querySelector('#root'));
