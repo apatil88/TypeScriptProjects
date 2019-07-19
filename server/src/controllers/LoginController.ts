db09e56772dabe59d7f404762d164ebb86533879
apatil88 <amrut.patil88@hotmail.com>
2019-07-19 16:38:00 -0400

    Adding root controller

--- server/src/controllers/LoginController.ts
+++ server/src/controllers/LoginController.ts
@@ -41,4 +41,10 @@ class LoginController {
       res.send("Invalid email or password");
     }
   }
+
+  @get("/logout")
+  getLogout(req: Request, res: Response) {
+    req.session = undefined;
+    res.redirect("/");
+  }
 }
